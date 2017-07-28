eb9b70b9f6df7c108abba83e728e53efa3aaf4bc
Ben <bensisson1@gmail.com>
2017-07-28 15:19:31 -0700

    replaced Object.assign with spread operator

--- ui/src/store/reducers/ChatScript/ChatScript.js
+++ ui/src/store/reducers/ChatScript/ChatScript.js
@@ -8,37 +8,27 @@ const ChatScript = (state = {
     past: []
 }, action = {}) => {
     if (action.type === "SET_ACTIVE_QUESTION_INDEX") {
-        const present = Object.assign(state.present, {
-            activeQuestionIndex: action.payload
-        });
+        const present = {...state.present, activeQuestionIndex: action.payload};
         return {...state, present};
     }
     if (action.type === "SET_QUESTIONS") {
-        const present = Object.assign(state.present, {
-            questions: action.payload
-        });
+        const present = {...state.present, questions: action.payload};
         return {...state, present};
     }
     if (action.type === "SET_QUESTION_ORDER") {
-        const present = Object.assign(state.present, {
-            questionOrder: action.payload
-        });
+        const present = {...state.present, questionOrder: action.payload};
         return {...state, present};
     }
     if (action.type === "ADD_TO_CHAT_HISTORY") {
-        const present = Object.assign(state.present, {
-            history: state.present.history.concat(action.payload)
-        });
+        const present = {...state.present, history: state.present.history.concat(action.payload)};
         return {...state, present};
     }
     if (action.type === "SAVE_STATE") {
-        const past = [Object.assign({}, state.present)].concat(state.past);
+        const past = [{...state.present}].concat(state.past.concat());
         return {...state, past};
     }
     if (action.type === "SET_COMPLETED_MESSAGE") {
-        const present = Object.assign(state.present, {
-            completedMessage: action.payload
-        });
+        const present = {...state.present, completedMessage:action.payload};
         return {...state, present};
     }
     if (action.type === "GO_BACK") {
